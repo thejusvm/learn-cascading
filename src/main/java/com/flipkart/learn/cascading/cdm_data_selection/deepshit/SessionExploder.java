9da0484c298430667bead6527e355853467ebfdf
thejus <thejus@flipkart.com>
2017-09-19 13:24:33 +0530

    [DS-1011] Changing the negative impression window to positions lower than clicked products

--- src/main/java/com/flipkart/learn/cascading/cdm_data_selection/deepshit/SessionExploder.java
+++ src/main/java/com/flipkart/learn/cascading/cdm_data_selection/deepshit/SessionExploder.java
@@ -8,11 +8,11 @@ import cascading.pipe.Each;
 import cascading.pipe.GroupBy;
 import cascading.pipe.Pipe;
 import cascading.pipe.assembly.Discard;
+import cascading.pipe.assembly.Retain;
 import cascading.tuple.Fields;
 import cascading.tuple.Tuple;
 import com.flipkart.learn.cascading.commons.cascading.PipeRunner;
 import com.flipkart.learn.cascading.commons.cascading.SimpleFlow;
-import com.flipkart.learn.cascading.commons.cascading.SimpleFlowRunner;
 import com.flipkart.learn.cascading.commons.cascading.subAssembly.JsonDecodeEach;
 import com.flipkart.learn.cascading.commons.cascading.subAssembly.JsonEncodeEach;
 
@@ -20,14 +20,16 @@ import java.io.Serializable;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static com.flipkart.learn.cascading.cdm_data_selection.DataFields.*;
+
 public class SessionExploder implements SimpleFlow {
 
     public static final String PAST_CLICKED_PRODUCTS = "pastClickedProducts";
     public static final String PAST_BOUGHT_PRODUCTS = "pastBoughtProducts";
     public static final String POSITIVE_PRODUCTS = "positiveProducts";
     public static final String NEGATIVE_PRODUCTS = "negativeProducts";
-    public static final String CONTEXT = "context";
-    public static final String CONTEXT_CLICK = "context.click";
+    public static final String ACTION = "action";
+    public static final String ACTION_CLICK = "action.click";
     public static final String RANDOM_ID = "random_id";
 
     private static int numProducts = 10;
@@ -37,10 +39,12 @@ public class SessionExploder implements SimpleFlow {
         Pipe pipe = new Pipe("session-exploder-pipe");
         Fields userContext = new Fields(SessionDataGenerator.USER_CONTEXT);
         pipe = new JsonDecodeEach(pipe, userContext, SearchSessions.class);
-        Fields expodedFields = new Fields(RANDOM_ID, PAST_CLICKED_PRODUCTS, PAST_BOUGHT_PRODUCTS, POSITIVE_PRODUCTS, NEGATIVE_PRODUCTS, CONTEXT);
-        pipe = new Each(pipe, userContext, new ExplodeSessions(expodedFields), Fields.RESULTS);
+        Fields expodedFields = new Fields(RANDOM_ID, _FINDINGMETHOD, ACTION, PAST_CLICKED_PRODUCTS, PAST_BOUGHT_PRODUCTS, POSITIVE_PRODUCTS, NEGATIVE_PRODUCTS);
+        pipe = new Each(pipe, userContext, new ExplodeSessions(expodedFields), Fields.ALL);
+        pipe = new Retain(pipe, Fields.merge(new Fields(_ACCOUNTID), expodedFields));
         pipe = new JsonEncodeEach(pipe, expodedFields);
         pipe = new GroupBy(pipe, new Fields(RANDOM_ID));
+        pipe = new Discard(pipe, new Fields(RANDOM_ID));
         return pipe;
     }
 
@@ -62,22 +66,25 @@ public class SessionExploder implements SimpleFlow {
                 List<ProductObj> boughtProducts = session.getBoughtProducts();
 
                 for (ProductObj clickedProduct : clickedProducts) {
+                    String findingMethod = clickedProduct.getFindingmethod();
                     int clickedProductPos = clickedProduct.getPosition();
                     ArrayList<ProductObj> impressionsSorted = new ArrayList<>(impressionProducts);
                     impressionsSorted
-                            .sort(Comparator.comparingInt(o -> Math.abs(o.getPosition() - clickedProductPos)));
+                            .sort(Comparator.comparingInt(o -> Math.abs(clickedProductPos - o.getPosition())));
                     List<String> finalPastClick = pastClick;
                     List<String> finalPastBought = pastBought;
                     List negativeForClicked = impressionsSorted
                             .stream()
-                            .filter(x -> !clickedProduct.getProductId().equals(x.getProductId()))
-                            .filter(x -> !finalPastClick.contains(x.getProductId()))
-                            .filter(x -> !finalPastBought.contains(x.getProductId()))
+                            .filter(product -> !product.isClick()) // removing if the product has been clicked
+                            .filter(product -> !clickedProduct.getProductId().equals(product.getProductId())) // removing the clicked product
+                            .filter(product -> !finalPastClick.contains(product.getProductId())) // removed if the product had been clicked in the history
+//                            .filter(product -> !finalPastBought.contains(product.getProductId()))
+                            .filter(product -> clickedProduct.getPosition() + 2 >= product.getPosition()) // removing if the product's position is 2 greater than the clicked product
                             .map(ProductObj::getProductId)
                             .limit(numProducts)
                             .collect(Collectors.toList());
                     pastClick = new ArrayList<>(pastClick);
-                    functionCall.getOutputCollector().add(new Tuple(UUID.randomUUID().toString(), pastClick, pastBought, clickedProduct.getProductId(), negativeForClicked, CONTEXT_CLICK));
+                    functionCall.getOutputCollector().add(new Tuple(UUID.randomUUID().toString(), findingMethod, ACTION_CLICK, pastClick, pastBought, clickedProduct.getProductId(), negativeForClicked));
                 }
 
                 pastClick = new ArrayList<>(pastClick);
