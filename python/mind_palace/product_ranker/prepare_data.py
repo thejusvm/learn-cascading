0d0fb0630185a02d6c4e4e3e5243daaae47cb45b
thejus <thejus@flipkart.com>
2017-10-11 14:08:57 +0530

    [DS-1011] prepare data to iteratively process each file instead of processing all in one go.

--- python/mind_palace/product_ranker/prepare_data.py
+++ python/mind_palace/product_ranker/prepare_data.py
@@ -10,27 +10,13 @@ import os
 def logBreak() :
     print "------------------------------------------"
 
-def preparedata(data_path, pad_text ="<pad>",
-                default_click_text = "<defaultclick>",
-                test_size = 0.2,
+def process_file(data_path,
+                productdict,
                 min_click_context = 0):
 
-    filenames = glob.glob(data_path)
+    df = pd.read_csv(data_path, sep="\t")
 
     start = time.clock()
-    list_ = []
-    for file_ in filenames:
-        df = pd.read_csv(file_, sep="\t")
-        list_.append(df)
-        print "processed file : " + file_
-    df = pd.concat(list_)
-    print "time taken by data read : " + str(time.clock() - start)
-
-    start = time.clock()
-    productdict = DictIntegerizer(default = pad_text)
-    productdict.get(pad_text)
-    if default_click_text is not None :
-        productdict.get(default_click_text)
 
     integerize = lambda x : productdict.get(x)
     df["positiveProductsInt"] = df["positiveProducts"].apply(integerize)
@@ -39,21 +25,15 @@ def preparedata(data_path, pad_text ="<pad>",
         strList = json.loads(jString)
         return map(integerize, strList)
 
-    df = df[df["pastClickedProducts"].apply(lambda x: len(json.loads(x)) > min_click_context)]
+    df = df[df["pastClickedProducts"].apply(lambda x: len(json.loads(x)) >= min_click_context)]
 
     # jsonListCols = ["negativeProducts", "pastClickedProducts", "pastBoughtProducts"]
     jsonListCols = ["negativeProducts", "pastClickedProducts"]
     for col in jsonListCols :
         df[str(col + "Int")] = df[col].map(jsonandintegerize)
 
-
-    # raw_data = df[["positiveProductsInt", "negativeProductsInt", "pastClickedProductsInt", "pastBoughtProductsInt"]]
     required_fields = ["positiveProductsInt", "negativeProductsInt", "pastClickedProductsInt"]
     raw_data = df[required_fields]
-    trainFrame, testFrame = train_test_split(raw_data, test_size = test_size)
-
-    # trainFrame = trainFrame[required_fields]
-    # testFrame = testFrame[required_fields]
 
     click_len = raw_data["pastClickedProductsInt"].map(lambda x : len(x))
     print "histogram of num context clicks : "
@@ -64,7 +44,7 @@ def preparedata(data_path, pad_text ="<pad>",
     print "data prep done"
     logBreak()
 
-    return productdict, trainFrame, testFrame
+    return raw_data
 
 
 def get_productdict_path(data_path):
@@ -80,23 +60,42 @@ def get_productdict(data_path) :
     with open(data_path, 'rb') as handle:
         return pickle.load(handle)
 
+def prepare_data(raw_data_path,
+                 processed_data_path,
+                 pad_text ="<pad>",
+                 default_click_text = "<defaultclick>"):
+
+    productdict = DictIntegerizer(default = pad_text)
+    productdict.get(pad_text)
+    if default_click_text is not None :
+        productdict.get(default_click_text)
+
+    filenames = glob.glob(raw_data_path)
+    start = time.clock()
+    counter = 0
+    for in_file in filenames:
+        logBreak()
+        print "start file processing : " + in_file + ", with dict size : " + str(productdict.currentCount)
+        pd = process_file(in_file, productdict)
+        print "end file processing : " + in_file + ", in " + str(time.clock() - start)
+        out_file = processed_data_path + "/part-" + str(counter)
+        print out_file
+        pd.to_csv(out_file, sep ="\t", index=False)
+        print "dumped content of " + in_file + " to " + out_file
+        counter += 1
+        logBreak()
+
+    return dict
+
+
 if __name__ == '__main__' :
     raw_data_path = "/home/thejus/workspace/learn-cascading/data/sessionExplode-201708.MOB" + "/part-*"
-    productdict, train, test = preparedata(raw_data_path, test_size=0.1)
-
-
-
-    processed_data_path = "/home/thejus/workspace/learn-cascading/data/sessionExplode-201708.MOB.processed.10split"
+    processed_data_path = "/home/thejus/workspace/learn-cascading/data/sessionExplode-201708.MOB.processed"
     os.makedirs(processed_data_path)
 
+    productdict = prepare_data(raw_data_path, processed_data_path)
     product_dict_file = get_productdict_path(processed_data_path)
     with open(product_dict_file, 'w+b') as handle:
         pickle.dump(productdict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    train_file = get_train_path(processed_data_path)
-    test_file = get_test_path(processed_data_path)
-
-    train.to_csv(train_file, sep = "\t", index=False)
-    test.to_csv(test_file, sep = "\t", index=False)
 
     logBreak()
