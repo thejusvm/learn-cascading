e918e9b280c006ef2a02cca54899aab27f7e4d95
thejus <thejus@flipkart.com>
2017-10-19 20:55:23 +0530

    [DS-1011] cooccurance only to be counted in search

--- data/fetch_cooccurance.sh
+++ data/fetch_cooccurance.sh
@@ -3,7 +3,7 @@
 pid=$1
 num=$2
 echo "======================poitive"
-cat sessionExplode-201708.MOB/part-* | grep -i "organic" | awk -F'\t' '$7 ~ /'$pid'/ {print $5" "$7}' | cut -d" " -f 1 | sort | uniq -c | sort -n -r -k 1 | head -n $num
+cat sessionExplode-201708.MOB/part-* | grep -i "search" | awk -F'\t' '$7 ~ /'$pid'/ {print $5" "$7}' | cut -d" " -f 1 | sort | uniq -c | sort -n -r -k 1 | head -n $num
 echo "======================negative"
-cat sessionExplode-201708.MOB/part-* | grep -i "organic" | awk '$7 ~ /'$pid'/ {print $0}'  | awk -F'\t' '{print $6" "$7}' | cut -d" " -f 1 | egrep -o '"[^"]+"' | sed "s|\"||g"| sort | uniq -c | sort -n -r -k 1 | head -n $num
+cat sessionExplode-201708.MOB/part-* | grep -i "search" | awk '$7 ~ /'$pid'/ {print $0}'  | awk -F'\t' '{print $6" "$7}' | cut -d" " -f 1 | egrep -o '"[^"]+"' | sed "s|\"||g"| sort | uniq -c | sort -n -r -k 1 | head -n $num
 
