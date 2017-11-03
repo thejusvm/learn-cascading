1b1c31a36a21d49824f2bf3cf606996d16136471
thejus <thejus@flipkart.com>
2017-11-03 17:23:48 +0530

    [DS-1011] /score Model version changable via api

--- python/mind_palace/product_ranker/training/run_model.py
+++ python/mind_palace/product_ranker/training/run_model.py
@@ -18,6 +18,7 @@ class Scorer :
 
     def __init__(self, model_path):
 
+        self.model_path = model_path
         dir = tc.getTraningContextDir(model_path)
 
         print "loading training context : " + dir
