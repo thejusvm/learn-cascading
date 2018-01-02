dc24272047f7434c01c8d098ed7c3d0c05666abe
thejus <thejus@flipkart.com>
2018-01-02 15:31:40 +0530

    [DS-1011] ClickstreamIterator_v2 a copy of ClickstreamIterator

---
+++ python/mind_palace/product_ranker/prepare_data_v2/clickstream_iterator_v2.py
@@ -0,0 +1,54 @@
+import glob
+import tensorflow as tf
+
+import mind_palace.product_ranker.constants as CONST
+from mind_palace.product_ranker.commons import generate_feature_names
+
+
+# def _deserialize_function(_example_protos):
+#     return [tf.deserialize_many_sparse(_example_proto, tf.int64) for _example_proto in _example_protos]
+
+
+def _parse_function(feature_names, features, example_proto):
+    parsed_features = tf.parse_single_example(example_proto, features)
+    # return parsed_features[feature_names[0]]
+    return tuple([parsed_features[feature_name].values for feature_name in feature_names])
+    # return tuple([tf.sparse_tensor_to_dense(parsed_features[feature_name]) for feature_name in feature_names])
+    # return tf.sparse_tensor_to_dense(parsed_features[feature_names[0]])
+
+class ClickstreamDataset :
+
+    def __init__(self, attributes, shuffle=True, batch_size=None):
+        self.filenames = tf.placeholder(tf.string, shape=[None])
+        self.dataset = tf.contrib.data.TFRecordDataset(self.filenames)
+        self.feature_names = generate_feature_names(attributes, CONST.TRAINING_COL_PREFIXES)
+        features = dict([[feature_name, tf.VarLenFeature(dtype=tf.int64)] for feature_name in self.feature_names])
+        self.dataset = self.dataset.map(lambda row : _parse_function(self.feature_names, features, row),
+                                        num_threads = 25, output_buffer_size = 100 * batch_size)
+
+        if shuffle :
+            self.dataset = self.dataset.shuffle(buffer_size=100000)
+        if batch_size != None :
+            padding_value = CONST.DEFAULT_DICT_KEYS.index(CONST.PAD_TEXT)
+            num_features = len(self.feature_names)
+            padding_values = tuple([tf.constant(padding_value, dtype=tf.int64) for _ in range(num_features)])
+            padded_shapes = tuple([[None] for _ in range(num_features)])
+            self.dataset = self.dataset.padded_batch(batch_size, padded_shapes = padded_shapes, padding_values=padding_values)
+
+        self.iterator = self.dataset.make_initializable_iterator()
+        self.get_next = self.iterator.get_next()
+
+    def initialize_iterator(self, sess, ctr_data_path):
+        sess.run(self.iterator.initializer, feed_dict={self.filenames : ctr_data_path})
+
+if __name__ == '__main__' :
+
+    path = glob.glob("/home/thejus/workspace/learn-cascading/data/sessionExplodeWithAttributes-201708.MOB.large.search.final/part-*")
+    sess = tf.Session()
+    attributes = ["productId", "brand", "vertical"]
+    attributes = ["productId"]
+    dataset = ClickstreamDataset(attributes, batch_size=10)
+    dataset.initialize_iterator(sess, path)
+    get_next = dataset.get_next
+    print get_next
+    print sess.run(get_next)
