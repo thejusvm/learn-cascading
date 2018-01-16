9a43d5d8f2f856e8a2fc5a5c0017ac007e165526
thejus <thejus@flipkart.com>
2018-01-16 12:20:35 +0530

    [DS-1011] Aggregate sessions to filter lifestyle products

--- src/main/java/com/flipkart/learn/cascading/cdm_data_selection/deepshit/AggregateSessions.java
+++ src/main/java/com/flipkart/learn/cascading/cdm_data_selection/deepshit/AggregateSessions.java
@@ -1,7 +1,17 @@
 package com.flipkart.learn.cascading.cdm_data_selection.deepshit;
 
+import cascading.operation.expression.ExpressionFilter;
+import cascading.pipe.Each;
 import cascading.pipe.Pipe;
+import cascading.pipe.assembly.Discard;
+import cascading.tuple.Fields;
+import com.flipkart.learn.cascading.cdm_data_selection.DataFields;
 import com.flipkart.learn.cascading.commons.cascading.PipeRunner;
+import com.flipkart.learn.cascading.commons.cascading.subAssembly.TransformEach;
+
+import java.util.Arrays;
+
+import static com.flipkart.learn.cascading.cdm_data_selection.deepshit.SessionDataGenerator.lifeStylePrefixes;
 
 public class AggregateSessions {
 
@@ -18,10 +28,18 @@ public class AggregateSessions {
         String input = args[0];
         String cmsInput = args[1];
         String output = args[2];
+
         Pipe pipe = new Pipe("aggregate_sessions");
+        Fields should_filter = new Fields("should_filter");
+        pipe = new TransformEach(pipe, new Fields(DataFields._PRODUCTID), should_filter,
+                pid -> SessionDataGenerator.isLifeStyle((String) pid) ? 1 : 0, Fields.ALL);
+        pipe = new Each(pipe, should_filter, new ExpressionFilter("(should_filter == 0)", Integer.class));
+        pipe = new Discard(pipe, should_filter);
+        pipe = SessionDataGenerator.aggregateSessionsPipe(pipe, cmsInput);
+
         PipeRunner runner = new PipeRunner("aggregate-sessions");
         runner.setNumReducers(2000);
-        runner.executeHfs(SessionDataGenerator.aggregateSessionsPipe(pipe, cmsInput), input, output, true);
+        runner.executeHfs(pipe, input, output, true);
 
     }
 
