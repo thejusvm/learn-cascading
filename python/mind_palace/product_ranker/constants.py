a89d85f7ef86a711a03e5afc21c96750224df547
thejus <thejus@flipkart.com>
2018-03-01 15:46:20 +0530

    [DS-1011] negative_with_random added to constants

--- python/mind_palace/product_ranker/constants.py
+++ python/mind_palace/product_ranker/constants.py
@@ -14,4 +14,4 @@ DEFAULT_DICT_KEYS = [PAD_TEXT, MISSING_DATA_TEXT, DEFAULT_CLICK_TEXT]
 
 # COL_PREFIXES = tuple([POSITIVE_COL_PREFIX, NEGATIVE_COL_PREFIX, CLICK_COL_PRERFIX, BOUGHT_COL_PREFIX])
 # TRAINING_COL_PREFIXES = tuple(["positive", "negative", "clicked", "bought"])
-TRAINING_COL_PREFIXES = tuple(["positive", "negative", "uniform_random_negative", "impression_random_negative", "clicked_short", "clicked_long", "bought"])
+TRAINING_COL_PREFIXES = tuple(["positive", "negative", "negative_with_random", "uniform_random_negative", "impression_random_negative", "clicked_short", "clicked_long", "bought"])
